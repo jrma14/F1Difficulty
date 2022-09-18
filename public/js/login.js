bdb34e79ae8c3bda831afa772fde01b5e54a6f46
Jacob <jrmadams02@gmail.com>
2022-09-17 21:07:16 -0400

    finally got local working

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,16 @@
 window.onload = (e) => {
-    let form = document.querySelector("#login")
-    form.onsubmit = (e) => {
-        e.preventDefault()
-        let data = {
-            username: document.querySelector('#username').value,
-            password: document.querySelector('#password').value
-        }
-        fetch('/login/username', {  
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data),
-        })
-        form.reset()
-    }
+    // let form = document.querySelector("#login")
+    // form.onsubmit = (e) => {
+    //     e.preventDefault()
+    //     let data = {
+    //         username: document.querySelector('#username').value,
+    //         password: document.querySelector('#password').value
+    //     }
+    //     fetch('/login/username', {  
+    //         method: 'post',
+    //         headers: {'Content-Type': 'application/json'},
+    //         body: JSON.stringify(data),
+    //     }).then(res => console.log).catch(e => console.log)
+    //     form.reset()
+    // }
 }
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { send } = require('process');
+
 const express = require('express'),
     app = express(),
     passport = require('passport'),
@@ -13,18 +15,19 @@ const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@f1difficultyc
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.use((req, res, next) => {
-    // console.log(req.url)
     // console.log(req.method)
+    // console.log(req.url)
     next()
 })
 // app.use(require('cookie-parser')());//might not be working because express-session automatically parses cookies
-app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({extended: true}));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));//automatically parses cookies
 app.use(express.static(path.join(__dirname, 'public'), { index: false, extensions: ['html'] }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
     // debugger
+    // console.log(req.user)
     if (!req.url.includes('/login')) {
         if (req.user) {
             next()
@@ -37,7 +40,6 @@ app.use((req, res, next) => {
     }
 })
 app.use(express.static(path.join(__dirname, 'protected'), { index: false, extensions: ['html'] }));
-
 
 passport.serializeUser(function (user, done) {
     done(null, user);
@@ -96,14 +98,18 @@ passport.use(new LocalStrategy(
             if (err) { return done(err); }
             if (!user) { return done(null, false); }
             if (!user.password === password) { return done(null, false); }
+            // console.log('Logged In')
+            // console.log(user)
             return done(null, user);
         });
     }))
 
-app.post('/login/username', passport.authenticate('local', { failureRedirect: '/login' }),
-    function (req, res) {
-        res.redirect('/');
-    });
+app.post('/login/username', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}),(req, res) => {
+    console.log(req)
+});
 
 app.get('/login/google', passport.authenticate('google'));
 
@@ -122,6 +128,8 @@ app.get('/login/auth/github/callback',
     })
 
 app.get('/', (req, res) => {
+    // console.log('getting /')
+    // console.log(req.user)
     ejs.renderFile('./protected/index.ejs', { loginText: req.user ? 'logout' : 'login', loginAction: req.user ? '/logout' : '/login' }, {}, (err, template) => {
         if (err) {
             throw err;
